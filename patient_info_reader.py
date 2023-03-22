477c279e1ec2d56a542be6f55d5975af2f6abdc9
nb963 <2003nitya@gmail.com>
2023-03-22 12:07:27 -0400

    new patient add case commit

--- patient_info_reader.py
+++ patient_info_reader.py
@@ -1,5 +1,16 @@
 f = open(patient_records.txt)
 all_lines = f.readlines()
 
+patient_records = {}
+
 for line in all_lines:
-    patient_record = line.split(" ")
+    change_record = line.split(" ")
+    if change_record[0] == 'ADD':
+        if change_record[1] == 'PATIENT':
+            identifier = change_record[2]
+            if identifier not in patient_records.keys():
+                patient_records[identifier] = [(change_record[3]+" "+change_record[3]), 0]
+                
+            
+            
+        
