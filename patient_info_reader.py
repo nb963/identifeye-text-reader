f33a29b2de8da6137c1f35e3f085a7c6ac7e22ca
nb963 <2003nitya@gmail.com>
2023-03-22 14:02:39 -0400

    delete patient and exam functionality added commit

--- patient_info_reader.py
+++ patient_info_reader.py
@@ -13,14 +13,25 @@ for line in all_lines:
                 patient_records[identifier] = [(change_record[3]+" "+change_record[4]), []]
                 
         elif change_record[1] == 'EXAM': 
-            identifier = change_record[2]
             if change_record[3] not in exams:
+                identifier = change_record[2]
                 if identifier in patient_records.keys():
                     patient_records[identifier][1].append(change_record[3])
                     exams.append(change_record[3])
                     
     elif change_record[0] == 'DELETE':
-        if 
+        if change_record[1] == 'PATIENT':
+            identifier = change_record[2]
+            if identifier in patient_records.keys():
+                patient_records.pop(identifier)
+                
+        elif change_record[1] == 'EXAM': 
+            if change_record[3] in exams:
+                identifier = change_record[2]
+                if identifier in patient_records.keys():
+                    patient_records[identifier][1].pop(change_record[3])
+                    exams.pop(change_record[3])
+            
         
         
                     
