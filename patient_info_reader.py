7406174afbf3695f0cd0eee4c8c183b56e8ce585
nb963 <2003nitya@gmail.com>
2023-03-24 11:39:07 -0400

    info print functionality added + bug fixes commit

--- patient_info_reader.py
+++ patient_info_reader.py
@@ -1,36 +1,43 @@
-f = open(patient_records.txt)
+f = open("patient_record.txt", "r")
 all_lines = f.readlines()
 
 patient_records = {}
 exams = []
 
 for line in all_lines:
+    line = line.strip()
     change_record = line.split(" ")
     if change_record[0] == 'ADD':
         if change_record[1] == 'PATIENT':
             identifier = change_record[2]
-            if identifier not in patient_records.keys():
+            if identifier not in patient_records:
                 patient_records[identifier] = [(change_record[3]+" "+change_record[4]), []]
                 
         elif change_record[1] == 'EXAM': 
             if change_record[3] not in exams:
                 identifier = change_record[2]
-                if identifier in patient_records.keys():
+                if identifier in patient_records:
                     patient_records[identifier][1].append(change_record[3])
                     exams.append(change_record[3])
                     
-    elif change_record[0] == 'DELETE':
+    elif change_record[0] == 'DEL':
         if change_record[1] == 'PATIENT':
             identifier = change_record[2]
-            if identifier in patient_records.keys():
+            if identifier in patient_records:
                 patient_records.pop(identifier)
                 
         elif change_record[1] == 'EXAM': 
             if change_record[3] in exams:
                 identifier = change_record[2]
-                if identifier in patient_records.keys():
+                if identifier in patient_records:
                     patient_records[identifier][1].pop(change_record[3])
                     exams.pop(change_record[3])
+                    
+                    
+for k in patient_records:
+    print("Name: " + (patient_records[k][0]) + ", Id: " + k + ", Exam Count: " + str(len(patient_records[k][1])))
+                
+                    
             
         
         
